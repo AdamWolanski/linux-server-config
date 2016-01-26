74b38fdc2f908506d07bc3293a027f4733834c74
Adam Wolanski <adam.wolanski@tieto.com>
2016-01-26 19:46:08 +0100

    bash extension fixed

--- bash_ext.sh
+++ bash_ext.sh
@@ -9,7 +9,7 @@ echo "if [ -f /etc/bash_completion ]; then
       . /etc/bash_completion
       fi
       export GIT_PS1_SHOWDIRTYSTATE=1
-      export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]\$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '" >> ~/.bashrc
+      export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\] $(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '" >> ~/.bashrc
 
 echo "set -g mouse-resize-pane on
       set -g mouse-select-pane on
